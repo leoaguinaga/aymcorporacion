57191a10d15ddd06dec89d424452feedbf69c19c
leoaguinaga <u21219471@utp.edu.pe>
2026-03-05 17:14:35 -0500

    feat: Introduce interactive services navigation, update header, and refresh site branding with new assets.

---
+++ src/components/ServicesHover.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button"
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { ChevronDown } from "lucide-react"
+
+export function ServicesHover() {
+    return (
+        <HoverCard openDelay={10} closeDelay={100}>
+            <HoverCardTrigger asChild>
+                <p className="hover:text-primary transition-colors duration-300 flex items-center gap-1 cursor-pointer">Servicios <ChevronDown className="size-4" /></p>
+            </HoverCardTrigger>
+            <HoverCardContent className="flex w-64 flex-col gap-2 z-9999" align="start">
+                <a href="venta-transporte-agregados" className="hover:text-primary transition-colors duration-300 font-medium">Venta y transporte de agregados</a>
+                <a href="alquiler-maquinaria-pesada" className="hover:text-primary transition-colors duration-300 font-medium">Alquiler de maquinaria pesada</a>
+                <a href="movimiento-tierras" className="hover:text-primary transition-colors duration-300 font-medium">Movimiento de tierras</a>
+                <a href="servicios-construccion" className="hover:text-primary transition-colors duration-300 font-medium">Servicios para construcción</a>
+            </HoverCardContent>
+        </HoverCard>
+    )
+}
